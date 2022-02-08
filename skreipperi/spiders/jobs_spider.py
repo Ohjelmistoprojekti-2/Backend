bf17d296297b59f8877bf5640c86d9080b882006
jazmc <jasmin.lumme@gmail.com>
2022-02-08 10:19:29 +0200

    Kommentteja & if-ehto tyhjille hakutuloksille

--- skreipperi/spiders/jobs_spider.py
+++ skreipperi/spiders/jobs_spider.py
@@ -5,19 +5,29 @@ class JobsSpider(scrapy.Spider):
     name = "jobs"
 
     def start_requests(self):
+        # array haettavista osoitteista
         urls = [
             'https://duunitori.fi/tyopaikat',
         ]
         for url in urls:
+            # haetaan "tagi" -a -flagin kanssa annetusta tags-attribuutista
             tag = getattr(self, 'tag', None)
+
+            # jos tagi on olemassa, lisätään se urliin
             if tag is not None:
                 url = url + '?haku=' + tag
+            # parsetaan urlin sisältö
             yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
+        # kun urleja tulee myöhemmin lisää, tähän tarvii ehdon että onko kyseessä Duunitori
+        # loopataan jokainen työpaikkadiv
         for job in response.css('div.job-box'):
-            yield {
-                'header': job.css('a.gtm-search-result::text').get(),
-                'company': job.css('a.gtm-search-result::attr(data-company)').get(),
-                'url':job.css('a.gtm-search-result::attr(href)').get(),
-            }
+            # jos työpaikkadivillä on jokin järkevä otsikko, huomioidaan työpaikka
+            if job.css('a.gtm-search-result::text').get() != None:
+                # assignataan divin sisältöelementtejä nimettyihin kenttiin
+                yield {
+                    'header': job.css('a.gtm-search-result::text').get(),
+                    'company': job.css('a.gtm-search-result::attr(data-company)').get(),
+                    'url':job.css('a.gtm-search-result::attr(href)').get(),
+                }
