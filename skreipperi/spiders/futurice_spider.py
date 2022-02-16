176bafa1f7d1e54ded209c7fdfa248ba76d9f514
Juho Rissanen <bgp040@myy.haaga-helia.fi>
2022-02-16 12:38:39 +0200

    Getting json data from scraper

--- skreipperi/spiders/futurice_spider.py
+++ skreipperi/spiders/futurice_spider.py
@@ -1,8 +1,14 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
+import json
+from json import JSONEncoder
 
+
+jobPosts = []
 
 class JobsSpider(scrapy.Spider):
     name = "jobs"
+
 
     def start_requests(self):
         urls = [
@@ -15,11 +21,39 @@ class JobsSpider(scrapy.Spider):
             yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
+
+        class Post:
+            def __init__(self, header, ala, company, url):
+                self.header = header
+                self.ala = ala
+                self.company = company
+                self.url = url
+
         for job in response.css('.css-ffhm6p'):
             if(job.css('.css-zsp0rz::text').get() == "Tech"):
-                yield {
-                    'header': job.css('.focusOnHover::text').get(),
-                    'ala': job.css('.css-zsp0rz::text').get(),
-                    'company': "Futurice",
-                    'url': "https://futurice.com/"+job.css('a::attr(href)').get(),
-                }
+
+                header = job.css('.focusOnHover::text').get()
+                ala = job.css('.css-zsp0rz::text').get()
+                company = "Futurice"
+                url = "https://futurice.com/"+job.css('a::attr(href)').get()
+
+                p1 = Post(header, ala, company, url)
+                print(p1.header)
+                jobPosts.append(p1)
+
+process = CrawlerProcess()
+process.crawl(JobsSpider)
+process.start()
+
+
+class JobEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__
+
+
+def printList():
+
+    jsonData = json.dumps(jobPosts, indent= 4, cls=JobEncoder)
+    print(jsonData)
+
+printList()
