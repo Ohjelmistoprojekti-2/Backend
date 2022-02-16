07259a3f6825dcb2970b82f4df572d54932a5ec4
Tsufel <aleksidaa@gmail.com>
2022-02-16 13:23:17 +0200

    Json muodon palautus visma_spiderille

--- skreipperi/spiders/visma_spider.py
+++ skreipperi/spiders/visma_spider.py
@@ -1,18 +1,50 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
+import json
+from json import JSONEncoder
 
-
-class JobsSpider(scrapy.Spider):
+jobPosts = []
+class VismaSpider(scrapy.Spider):
     name = "visma_jobs"
     start_urls = ['https://ats.talentadore.com/positions/rJ4tqSR/rss?v=2&language=en%2Cfi&tags=&notTags=Visma+Legal+avoin+hakemus&businessUnits=&notBusinessUnits=&display_description=job_description&categories=tags_and_extras']
+    
     
 
     def parse(self, response):
         
+        class Post:
+            def __init__(self, header, ala, company, url):
+                self.header = header
+                self.ala = ala
+                self.company = company
+                self.url = url
+        
+
+
         for job in response.xpath('//channel/item'):
-            yield {
-                'header' : job.xpath('title//text()').extract_first(),
-                'company': job.xpath('dc.creator//text()').extract_first(),  ##palauttaa vielä tyhjän listan
-                'url': job.xpath('link//text()').extract_first(),
-                
-            }
-      
+            
+              header = job.xpath('title//text()').extract_first()
+              ala = 'Tech'
+              company = job.xpath('dc.creator//text()').extract_first()
+              url =  job.xpath('link//text()').extract_first()
+
+              new = Post(header, ala, company, url)   
+              jobPosts.append(new)
+              
+              
+process = CrawlerProcess()
+process.crawl(VismaSpider)
+process.start()
+
+
+class JobEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__
+
+
+def printList():  
+    jsonData = json.dumps(jobPosts, indent= 4, cls=JobEncoder)
+    print(jsonData)
+    return jsonData
+
+printList()           
