80da38719235d0061519f2f461d5a7bc09120744
tia <tia.venalainen@gmail.com>
2022-03-15 09:33:33 +0200

    siili with location

--- skreipperi/spiders/jobs_siili.py
+++ skreipperi/spiders/jobs_siili.py
@@ -11,6 +11,7 @@ class Post(scrapy.Item):
     header = scrapy.Field()
     ala = scrapy.Field()
     company = scrapy.Field()
+    location = scrapy.Field()
     text = scrapy.Field()
     url = scrapy.Field()
 
@@ -25,24 +26,28 @@ class SiiliSpider(scrapy.Spider):
             header = job.css('h4 a::text').extract_first()
             ala = 'Tech'
             company = 'Siili Solutions'
+            locationLong = job.css('span.paragraph-3.siili__color--grey.c-careers-city::text').extract_first()
+            location = locationLong.split("/")[0]
             url = "https://siili.com"+job.css('a.c-careers__link::attr(href)').extract_first()
 
             request = scrapy.Request(url, callback=self.parse_following_page)
             request.cb_kwargs['header'] = header
             request.cb_kwargs['ala'] = ala
             request.cb_kwargs['company'] = company
+            request.cb_kwargs['location'] = location
             request.cb_kwargs['url'] = url
             yield request
 
-    def parse_following_page(self, response, header, ala, company, url):
+    def parse_following_page(self, response, header, ala, company, location, url):
         header = header
         ala = ala
         company = company
+        location = location
         url = url
         text = response.css('div.span6.c-careers__content p::text').getall()
 
             # if 'Java' in text:
-        new = Post(header=header, ala=ala, company=company, text=text, url=url)
+        new = Post(header=header, ala=ala, company=company, location=location, text=text, url=url)
         jobPosts.append(new)
 
 process = CrawlerProcess()
